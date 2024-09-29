825a50dab273917a139bccdef8c10a4ebac49918
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-09-28 21:53:56 -0700

    Finished the text part of the project, now all we have to do is add images, and resize correctly.

--- src/menu.js
+++ src/menu.js
@@ -14,10 +14,32 @@ function Menu()
     info_section.classList.add('menu-grid');
     info_section.classList.remove("home-grid");
     info_section.classList.remove("contact-grid");
-    for(let i = 0; i < 6; i++)
+
+    let pizza_types = ["Apple Pie: Your sweet delight, fruity flavor in the world of Pies!", "Mango Pie: Your sweet delight, fruity flavor in the world of Pies!", "Pumpkin Pie: Your sweet delight, fruity flavor in the world of Pies!", "Cherry Pie: Your sweet delight, fruity flavor in the world of Pies!"];
+
+    for(let i = 0; i < 4; i++)
     {
         const gridItem = document.createElement('div');
+        const description = document.createElement("div");
+        description.className = "item_description";
+        if(i === 0)
+        {
+            description.textContent = pizza_types[0];    
+        }
+        if(i === 1)
+        {
+            description.textContent = pizza_types[1];    
+        }
+        if(i === 2)
+        {
+            description.textContent = pizza_types[2];    
+        }
+        if(i === 3)
+        {
+            description.textContent = pizza_types[3];    
+        }
         gridItem.className = "menu_item";
+        gridItem.appendChild(description);
         info_section.appendChild(gridItem);
     }
 }
