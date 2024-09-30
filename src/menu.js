f78a6827ecd6e830f3c9ffc624c43826cc6ccc51
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-09-29 21:19:34 -0700

    Finished the project, I will come back later and work on responsiveness, but for now, we got it working. :)

--- src/menu.js
+++ src/menu.js
@@ -15,30 +15,35 @@ function Menu()
     info_section.classList.remove("home-grid");
     info_section.classList.remove("contact-grid");
 
-    let pizza_types = ["Apple Pie: Your sweet delight, fruity flavor in the world of Pies!", "Mango Pie: Your sweet delight, fruity flavor in the world of Pies!", "Pumpkin Pie: Your sweet delight, fruity flavor in the world of Pies!", "Cherry Pie: Your sweet delight, fruity flavor in the world of Pies!"];
+    let pie_types = ["Apple Pie: Your sweet delight, fruity flavor in the world of Pies!", "Mango Pie: Your sweet delight, fruity flavor in the world of Pies!", "Pumpkin Pie: Your sweet delight, fruity flavor in the world of Pies!", "Cherry Pie: Your sweet delight, fruity flavor in the world of Pies!"];
 
     for(let i = 0; i < 4; i++)
     {
         const gridItem = document.createElement('div');
         const description = document.createElement("div");
+        const image = document.createElement("img");
+        image.className = "item_image";
         description.className = "item_description";
         if(i === 0)
         {
-            description.textContent = pizza_types[0];    
+            description.textContent = pie_types[0];
+            image.style.backgroundImage = "/imgs/mango_pie.png";
         }
         if(i === 1)
         {
-            description.textContent = pizza_types[1];    
+            description.textContent = pie_types[1];   
+            image.style.backgroundImage = "/imgs/mango_pie.png"; 
         }
         if(i === 2)
         {
-            description.textContent = pizza_types[2];    
+            description.textContent = pie_types[2];    
         }
         if(i === 3)
         {
-            description.textContent = pizza_types[3];    
+            description.textContent = pie_types[3];    
         }
         gridItem.className = "menu_item";
+        gridItem.appendChild(image);
         gridItem.appendChild(description);
         info_section.appendChild(gridItem);
     }
